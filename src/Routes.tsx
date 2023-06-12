d5f42568c2999f6d030d2a43472e4c22a68bd3fc
mgckaled <marcelgckaled@gmail.com>
2023-06-12 13:58:57 -0300

    feat: config initial routers with react-router-dom

---
+++ src/Routes.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+
+import { HomePage } from "./pages/Home"
+import { CompleteOrderPage } from "./pages/CompleteOrder/Index"
+
+export function Router() {
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={<HomePage />}
+      />
+      <Route
+        path='/completeOrder'
+        element={<CompleteOrderPage />}
+      />
+    </Routes>
+  )
+}
