d2fda5dd0204ba1c28c03179936812994ea2e3f4
mgckaled <marcelgckaled@gmail.com>
2023-06-13 11:51:56 -0300

    feat: create coffee and its containers. add it to Home page

---
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -0,0 +1,76 @@
+import { ShoppingCart } from "phosphor-react"
+import { useState } from "react"
+
+import { QuantityInput } from "../../../../components/QuantityInput"
+import { RegularText, TitleText } from "../../../../components/Typography"
+import { formatMoney } from "../../../../utils/formatMoney"
+
+import { AddCartWrapper, CardFooter, CoffeeCardContainer, Description, Name, Tags } from "./styles"
+
+export interface Coffee {
+  id: number
+  tags: string[]
+  name: string
+  description: string
+  photo: string
+  price: number
+}
+
+interface CoffeeProps {
+  coffee: Coffee
+}
+
+export function CoffeeCard({ coffee }: CoffeeProps) {
+  const [quantity, setQuantity] = useState(1)
+
+  function handleIncrease() {
+    setQuantity(state => state + 1)
+  }
+
+  function handleDecrease() {
+    setQuantity(state => state - 1)
+  }
+
+  const formattedPrice = formatMoney(coffee.price)
+
+  return (
+    <CoffeeCardContainer>
+      <img src={`/coffees/${coffee.photo}`} />
+      <Tags>
+        {coffee.tags.map(tag => (
+          <span key={`${coffee.id}${tag}`}>{tag}</span>
+        ))}
+      </Tags>
+
+      <Name>{coffee.name}</Name>
+      <Description>{coffee.description}</Description>
+
+      <CardFooter>
+        <div>
+          <RegularText size='s'>R$</RegularText>
+          <TitleText
+            size='m'
+            color='text'
+            as='strong'
+          >
+            {formattedPrice}
+          </TitleText>
+        </div>
+
+        <AddCartWrapper>
+          <QuantityInput
+            onIncrease={handleIncrease}
+            onDecrease={handleDecrease}
+            quantity={quantity}
+          />
+          <button>
+            <ShoppingCart
+              weight='fill'
+              size={22}
+            />
+          </button>
+        </AddCartWrapper>
+      </CardFooter>
+    </CoffeeCardContainer>
+  )
+}
